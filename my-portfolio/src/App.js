73e5bf6c6ae7324c0546f5e015e03ab27e6c4668
millyanne93 <nmillyanne20@gmail.com>
2024-09-25 00:44:20 +0300

    Configure deployment to GitHub Pages

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'; 
 import Home from './pages/Home';
